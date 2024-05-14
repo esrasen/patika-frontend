0051aa5bccbf508546a82b599587b88410308855
Esra Şen <sen.esra@yahoo.com>
2024-05-14 23:00:37 +0300

    Added function to receipt.jsx

--- week-8/spend-clone/spend-clone/src/components/Receipt/receipt.jsx
+++ week-8/spend-clone/spend-clone/src/components/Receipt/receipt.jsx
@@ -3,6 +3,12 @@ import "./receipt.css";
 import ListItem from "./ListItem.jsx";
 
 const Receipt = ({ products, totalMoney }) => {
+
+    //listedeki ürün yoksa receipt kısmını gizle
+    if (Object.keys(products).length === 0) {
+        return null;
+    }
+
     return (
         <div className="receipt">
             <h2>Your Receipt</h2>
